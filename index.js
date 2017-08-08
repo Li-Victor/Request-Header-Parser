6f051ce4e05e885e290adf034d06ddee3e623fdb
Victor Li <victorli5674@yahoo.com>
2017-08-08 00:48:30 -0700

    got the request's ip, language, and software

---
+++ index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.enable('trust proxy');
+
+app.get('/', function(req, res, next) {
+	var regex = new RegExp(/\(([^)]+)\)/);
+	return res.status(200).send({
+		ipaddress: req.ip,
+    	language: req.headers['accept-language'] ? req.headers['accept-language'].split(',')[0] : 'Cannot read languages',
+		software: regex.exec(req.headers['user-agent']) ? (regex.exec(req.headers['user-agent']))[1] : req.headers['user-agent']
+	});
+});
+
+var port = 3000;
+app.listen(port, function() {
+	console.log('Listening on port ' + port);
+});
